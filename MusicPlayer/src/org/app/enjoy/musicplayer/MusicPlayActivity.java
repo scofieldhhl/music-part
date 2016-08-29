39f571ee693a24cc605dceeae895afdb7ba7dfb6
scofieldhhl <scofield.hhl@gmail.com>
2016-08-29 23:40:58 +0800

    update play iso with sdk6.0

--- MusicPlayer/src/org/app/enjoy/musicplayer/MusicPlayActivity.java
+++ MusicPlayer/src/org/app/enjoy/musicplayer/MusicPlayActivity.java
@@ -1,11 +1,14 @@
 package org.app.enjoy.musicplayer;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.media.AudioManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
@@ -45,6 +48,7 @@ import java.util.Observer;
 
 public class MusicPlayActivity extends BaseActivity implements View.OnClickListener,Observer{
 
+    public  int  REQUEST_CODE_SOME_FEATURES_PERMISSIONS = 1;
     private MusicListFragment musicListFragment;
     private MusicPlayFragment musicPlayFragment;
     private List<Fragment> frags = new ArrayList<>();
@@ -87,10 +91,57 @@ public class MusicPlayActivity extends BaseActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_play);
+        checkSDK();
         mContext = this;
         initialize();
         LoadMenu();
 
+    }
+
+    private void checkSDK(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            int hasWritePermission = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            int hasReadPermission = checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
+
+            List<String> permissions = new ArrayList<String>();
+            if (hasWritePermission != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            } else {
+//                preferencesUtility.setString("storage", "true");
+            }
+
+            if (hasReadPermission != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+
+            } else {
+//                preferencesUtility.setString("storage", "true");
+            }
+
+            if (!permissions.isEmpty()) {
+                requestPermissions(permissions.toArray(new String[permissions.size()]), REQUEST_CODE_SOME_FEATURES_PERMISSIONS);
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                for (int i = 0; i < permissions.length; i++) {
+                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                        System.out.println("Permissions --> " + "Permission Granted: " + permissions[i]);
+                    } else if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                        System.out.println("Permissions --> " + "Permission Denied: " + permissions[i]);
+                    }
+                }
+            }
+            break;
+            default: {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
     }
 
     private void initialize () {
